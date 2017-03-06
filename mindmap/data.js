551cc2dc861d392fd86d84a8d43e0602e5ebb336
Mateusz Klimek <mklimus@gmail.com>
2017-03-06 21:35:32 +0000

    initial version of mindmap

---
+++ mindmap/data.js
@@ -0,0 +1,28 @@
+var json = {
+   "id":"347_0",
+   "name":"Machine Learning",
+   "children":[
+      {
+         "id":"126510_1",
+         "name":"Supervisted Learning",
+         "data":{
+            "band":"Nine Inch Nails",
+            "relation":"member of band",
+            "other": "coś tam ode mnie"
+         }
+      },
+      {
+         "id":"173871_4",
+         "name":"Unsupervised Learning",
+         "data":{
+            "band":"Nine Inch Nails",
+            "relation":"member of band"
+         },
+         "children":[
+
+         ]
+      }],
+   "data":[
+
+   ]
+};
---
+++ mindmap/graph.js
@@ -0,0 +1,101 @@
+var labelType, useGradients, nativeTextSupport, animate;
+
+(function() {
+  var ua = navigator.userAgent,
+      iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
+      typeOfCanvas = typeof HTMLCanvasElement,
+      nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
+      textSupport = nativeCanvasSupport 
+        && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+  labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
+  nativeTextSupport = labelType == 'Native';
+  useGradients = nativeCanvasSupport;
+  animate = !(iStuff || !nativeCanvasSupport);
+})();
+
+var Log = {
+  elem: false,
+  write: function(text){
+    // if (!this.elem) 
+    //   this.elem = document.getElementById('log');
+    // this.elem.innerHTML = text;
+    // this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
+  }
+};
+
+function init(){
+    
+    var infovis = document.getElementById('infovis');
+    var w = infovis.offsetWidth - 50, h = infovis.offsetHeight - 50;
+    
+    var ht = new $jit.Hypertree({
+      //id of the visualization container
+      injectInto: 'infovis',
+      width: w,
+      height: h,
+      Node: {
+          dim: 13,
+          color: "#f00"
+      },
+      Edge: {
+          lineWidth: 2,
+          color: "#088"
+      },
+      onBeforeCompute: function(node){
+          Log.write("centering");
+      },
+      onCreateLabel: function(domElement, node){
+          domElement.innerHTML = node.name;
+          $jit.util.addEvent(domElement, 'click', function () {
+              ht.onClick(node.id, {
+                  onComplete: function() {
+                      ht.controller.onComplete();
+                  }
+              });
+          });
+      },
+      onPlaceLabel: function(domElement, node){
+          var style = domElement.style;
+          style.display = '';
+          style.cursor = 'pointer';
+          if (node._depth <= 1) {
+              style.fontSize = "1.2em";
+              style.color = "#ddd";
+
+          } else if(node._depth == 2){
+              style.fontSize = "0.7em";
+              style.color = "#555";
+
+          } else {
+              style.display = 'none';
+          }
+
+          var left = parseInt(style.left);
+          var w = domElement.offsetWidth;
+          style.left = (left - w / 2) + 'px';
+      },
+      
+      onComplete: function(){
+          Log.write("done");
+          
+          //Build the right column relations list.
+          //This is done by collecting the information (stored in the data property) 
+          //for all the nodes adjacent to the centered node.
+          var node = ht.graph.getClosestNodeToOrigin("current");
+          var html = "<h2>" + node.name + "</h2><b>Information:</b><br/>todo";
+          html += "<ul>";
+          node.eachAdjacency(function(adj){
+              var child = adj.nodeTo;
+              if (child.data) {
+                  var rel = (child.data.band == node.name) ? child.data.relation : node.data.relation;
+                  //html += "<li>" + child.name + " " + "<div class=\"relation\">(relation: " + rel + ")</div></li>";
+              }
+          });
+          html += "</ul>";
+          $jit.id('inner-details').innerHTML = html;
+      }
+    });
+    ht.loadJSON(json);
+    ht.refresh();
+    ht.controller.onComplete();
+}
